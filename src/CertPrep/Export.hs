3f91d62834b2d3e59b76b9da9ad291b3b2ae9ea7
Paul Grillenberger <grillenbergerpaul@gmail.com>
2026-07-25 15:24:23 +0200

    Add json export

--- src/CertPrep/Export.hs
+++ src/CertPrep/Export.hs
@@ -1,8 +1,24 @@
 module CertPrep.Export where
 
+import Data.Aeson (encode)
+import Data.Aeson.Types (ToJSON)
+import Data.ByteString.Lazy (LazyByteString)
+
 import CertPrep.Types (Answer, Question)
 
 data ExportFormat = Json | Markdown
 
-export :: ExportFormat -> [(Question, Answer)] -> Text
-export = undefined
+export :: ExportFormat -> [(Question, Answer)] -> LazyByteString
+export Json = exportJson
+export Markdown = undefined
+
+data ExamJsonExportQAPair = ExamJsonExportQAPair {
+  question :: Question,
+  answer :: Answer
+}
+  deriving (Show, Eq, Generic)
+
+instance ToJSON ExamJsonExportQAPair
+
+exportJson :: [(Question, Answer)] -> LazyByteString
+exportJson = encode . fmap (uncurry ExamJsonExportQAPair)
